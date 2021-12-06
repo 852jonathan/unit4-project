ca0d305d707e9cb84995560c1208de03d68e511c
gitjonathan <yootwodeetwo@gmail.com>
2021-12-06 15:06:25 +0800

    pushed to heroku2

--- src/db/seeders/seedProducts.js
+++ src/db/seeders/seedProducts.js
@@ -14,104 +14,134 @@ module.exports = {
         description: 'Classic beef cheeseburger with cheese, lettuce & tomatoes',
         price: 55,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/classic-cheeseburger.JPG'})
+        image: '/assets/menu/classic-cheeseburger.JPG',
+        feature: true
+    })
 
       await Product.create({
         productName: 'Double Cheeseburger with Mushrooms',
         description: 'Double beef cheeseburger with cheese, lettuce, tomatoes & mushrooms',
         price: 70,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/double-cheeseburger.JPG' })
+        image: '/assets/menu/double-cheeseburger.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Christmas Burger',
         description: 'Beef burger with a colorful mix of vegetables',
         price: 68,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/christmas-burger.JPG' })
+        image: '/assets/menu/christmas-burger.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Squid Ink Bun with Bacon',
         description: 'Squid Ink Bun With Bacon And Cheese',
         price: 65,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/squid-ink-bun-with-bacon.JPG'})
+        image: '/assets/menu/squid-ink-bun-with-bacon.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Color Combo Burger',
         description: 'A colorful mix of yummy flavors',
         price: 60,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/color-combo-burger.JPG'})
+        image: '/assets/menu/color-combo-burger.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Gourmet Cheeseburger',
         description: 'A flavorful combination with swiss cheese and double onions!',
         price: 128,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/gourmet-cheeseburger.JPG'})
+        image: '/assets/menu/gourmet-cheeseburger.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Mixbun Burger',
         description: 'Give this Dual Bun Burger A Try!',
         price: 90,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/mixbun-burger.JPG'})
+        image: '/assets/menu/mixbun-burger.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Pickle Beef Burger',
         description: 'Pickle Burger With Tomato',
         price: 55,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/pickle-beef-burger.JPG' })
+        image: '/assets/menu/pickle-beef-burger.JPG' ,
+        feature: true
+      })
 
       await Product.create({
         productName: 'Mushroom Burger',
         description: 'A Delicious Combination Of Mushroom And Rocket Leaf',
         price: 60,
         CategoryId: burgers[0].id,
-        image: '/assets/menu/mushroom-burger.JPG' })
+        image: '/assets/menu/mushroom-burger.JPG' ,
+        feature: true
+      })
 
       await Product.create({
         productName: 'French Fries',
         description: 'A classic side to go with your burger, with ketchup!',
         price: 20,
         CategoryId: sides[0].id,
-        image: '/assets/menu/fries.JPG'})
+        image: '/assets/menu/fries.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Potato Slices',
         description: 'Deep fried to perfection',
         price: 20,
         CategoryId: sides[0].id,
-        image: '/assets/menu/potato-slices.JPG' })
+        image: '/assets/menu/potato-slices.JPG' ,
+        feature: true
+      })
 
       await Product.create({
         productName: 'Potato Wedges',
         description: 'Large wedges with a crunchy texture',
         price: 25,
         CategoryId: sides[0].id,
-        image: '/assets/menu/potato-wedges.JPG'})
+        image: '/assets/menu/potato-wedges.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Red Cola',
         description: 'Red Cola: A classic soda',
         price: 18,
         CategoryId: drinks[0].id,
-        image: '/assets/menu/red-cola.JPG' })
+        image: '/assets/menu/red-cola.JPG' ,
+        feature: true
+      })
 
       await Product.create({
         productName: 'Green Can Soda',
         description: 'Green Can Soda: A green can classic',
         price: 18,
         CategoryId: drinks[0].id,
-        image: '/assets/menu/green-soda.JPG'})
+        image: '/assets/menu/green-soda.JPG',
+        feature: true
+      })
 
       await Product.create({
         productName: 'Strawberry Milkshake',
         description: 'Great with burgers!',
         price: 25,
         CategoryId: drinks[0].id,
-        image: '/assets/menu/strawberry-milkshake.JPG'})
-  }
+        image: '/assets/menu/strawberry-milkshake.JPG',
+        feature: true
+      })
+    }
 }
