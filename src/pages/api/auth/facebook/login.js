56bc055688a5787c37d808fc32e0a9668de88162
gitjonathan <yootwodeetwo@gmail.com>
2021-12-06 18:31:21 +0800

    push to heroku

--- src/pages/api/auth/facebook/login.js
+++ src/pages/api/auth/facebook/login.js
@@ -1,36 +1,36 @@
-// Redirect the user to Facebook for authentication.  When complete,
-// Facebook will redirect the user back to the application at
-//     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook'))
+// // Redirect the user to Facebook for authentication.  When complete,
+// // Facebook will redirect the user back to the application at
+// //     /auth/facebook/callback
+// app.get('/auth/facebook', passport.authenticate('facebook'))
 
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-app.get(
-  '/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/menu',
-    failureRedirect: '/' })
-)
+// // Facebook will redirect the user to this URL after approval.  Finish the
+// // authentication process by attempting to obtain an access token.  If
+// // access was granted, the user will be logged in.  Otherwise,
+// // authentication has failed.
+// app.get(
+//   '/auth/facebook/callback',
+//   passport.authenticate('facebook', { successRedirect: '/menu',
+//     failureRedirect: '/' })
+// )
 
-passport.use(new FacebookStrategy(strategyOptions, verifyCallback))
+// passport.use(new FacebookStrategy(strategyOptions, verifyCallback))
 
-+  app.get(`${process.env.BASE_API_URL}/auth/facebook`, passport.authenticate('facebook'))
-+
-+  app.get(
-+    `${process.env.BASE_API_URL}/auth/facebook/callback`,
-+    passport.authenticate('facebook', { failureRedirect: '/' }),
-+    (req, res) => {
-+      return res
-+        .status(200)
-+        .cookie('jwt', signToken(req.user), {
-+          httpOnly: true
-+        })
-+        .redirect('/menu')
-+    }
-+  )
+// +  app.get(`${process.env.BASE_API_URL}/auth/facebook`, passport.authenticate('facebook'))
+// +
+// +  app.get(
+// +    `${process.env.BASE_API_URL}/auth/facebook/callback`,
+// +    passport.authenticate('facebook', { failureRedirect: '/' }),
+// +    (req, res) => {
+// +      return res
+// +        .status(200)
+// +        .cookie('jwt', signToken(req.user), {
+// +          httpOnly: true
+// +        })
+// +        .redirect('/menu')
+// +    }
+// +  )
 
-  return app
-}
+//   return app
+// }
 
-export { strategy }
+// export { strategy }
